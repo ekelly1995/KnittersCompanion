2b8aa075e20fdc04ffbc44af8c603fca84e845bb
Emily Kelly <emilykelly1995@outlook.com>
2022-01-03 21:22:47 -0700

    Changed data binding on Counter page buttons to normal button logic, will update to data binding again at a later time

--- KnittersCompanion/KnittersCompanion/ViewModels/CounterViewModel.cs
+++ KnittersCompanion/KnittersCompanion/ViewModels/CounterViewModel.cs
@@ -10,31 +10,7 @@ namespace KnittersCompanion.ViewModels
         public CounterViewModel()
         {
             Title = "StitchRowCounter";
-            IncreaseCount = new Command(OnIncrease);
-            //BindingContext = this;
         }
 
-        public ICommand IncreaseCount { get; }
-
-        int count = 0;
-        string countDisplay = "0";
-        
-        public string CountDisplay
-        {
-            get => countDisplay;
-            set
-            {
-                if (value == countDisplay)
-                    return;
-                countDisplay = value;
-                OnPropertyChanged();
-            }
-        }
-
-        void OnIncrease()
-        {
-            count++;
-            CountDisplay = $"{count}";
-        }
     }
 }
--- KnittersCompanion/KnittersCompanion/Views/CounterPage.xaml.cs
+++ KnittersCompanion/KnittersCompanion/Views/CounterPage.xaml.cs
@@ -16,5 +16,48 @@ namespace KnittersCompanion.Views
         {
             InitializeComponent();
         }
+
+        private void Button_IncreaseRow(object sender, EventArgs e)
+        {
+            string currentText = RowCount.Text;
+            int newCount = Convert.ToInt32(currentText) + 1;
+            RowCount.Text = Convert.ToString(newCount);
+        }
+
+        private void Button_DecreaseRow(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(RowCount.Text) == 0)    // If the value is already at 0, we don't want to go negative
+            {
+                RowCount.Text = RowCount.Text;
+            }
+            else
+            {
+                string currentText = RowCount.Text;
+                int newCount = Convert.ToInt32(currentText) - 1;
+                RowCount.Text = Convert.ToString(newCount);
+            }
+        }
+
+        private void Button_IncreaseStitch(object sender, EventArgs e)
+        {
+            string currentText = StitchCount.Text;
+            int newCount = Convert.ToInt32(currentText) + 1;
+            StitchCount.Text = Convert.ToString(newCount);
+        }
+
+        private void Button_DecreaseStitch(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(StitchCount.Text) == 0) // If the value is already at 0, we don't want to go negative
+            {
+                StitchCount.Text = StitchCount.Text;
+            }
+            else
+            {
+                string currentText = StitchCount.Text;
+                int newCount = Convert.ToInt32(currentText) - 1;
+                StitchCount.Text = Convert.ToString(newCount);
+            }
+        }
+
     }
 }
